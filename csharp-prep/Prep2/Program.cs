449e9c8809fb8cdfc0305541d83e37e50ed70915
Alejandro Vanderhorst <van26003@byui.edu>
2026-09-18 18:53:26 -0600

    Completed C# prep 2 exercise

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        
+        Console.Write("What is the students grade percentage? ");
+        int grade = int.Parse(Console.ReadLine());
+        bool pass = grade >= 70;
+        string letter = CalculateGrade(grade);
+        Console.Write($"The students grade letter is {letter} \n");
+        Console.Write($"The student has {(pass ? "Passed" : "Failed")} the class!");
+        
+    }
+
+    public static string CalculateGrade(int studentGrade)
+    {
+        bool pass = studentGrade >= 70;
+        (string letter, int grade)[] grades =
+        {
+            ("A" , 90),
+            ("B", 80),
+            ("C", 70),
+            ("D", 60),
+            ("F", 0)
+        };
+
+        foreach (var letters in grades)
+        {
+            if (studentGrade >= letters.grade)
+            {
+                return letters.letter;
+            }
+        }
+
+        return "F";
     }
 }
